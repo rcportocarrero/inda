d94953a4538b4a0fbff5ee75066c90f8614b45c5
Carlos Miguel Sing Ramos <carlossing@gmail.com>
2016-07-04 18:03:41 -0500

    FIX: Dashboard

--- module/Dashboard/src/Dashboard/Controller/IndexController.php
+++ module/Dashboard/src/Dashboard/Controller/IndexController.php
@@ -7,6 +7,7 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends \BaseX\Controller\BaseController {
 
+    protected $_need_auth = true;
     public function indexAction() {
         
         var_dump($this->getSessionStorage()->isAuthenticate());
