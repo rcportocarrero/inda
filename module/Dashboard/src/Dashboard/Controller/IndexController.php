b80041e18a0f6f173439770988b663f52a430eff
Carlos Miguel Sing Ramos <carlossing@gmail.com>
2016-07-04 17:50:29 -0500

    FIX

--- module/Dashboard/src/Dashboard/Controller/IndexController.php
+++ module/Dashboard/src/Dashboard/Controller/IndexController.php
@@ -5,9 +5,11 @@ namespace Dashboard\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController {
+class IndexController extends \BaseX\Controller\BaseController {
 
     public function indexAction() {
+        
+        var_dump($this->getSessionStorage()->isAuthenticate());
         
     }
 
