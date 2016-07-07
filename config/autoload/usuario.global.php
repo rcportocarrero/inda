a38f65a36b19ba1fb719400eaf212d5e653f3a24
Roy Cespedes <rcportocarrero@gmail.com>
2016-07-07 14:27:01 -0500

    fx

---
+++ config/autoload/usuario.global.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+return [
+    'usuario' => [
+        'captcha' => [
+            'enabled' => true,
+            'diccionario' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            'tamano_codigo' => 5
+        ],
+        'login' => [
+            'tamanio' => [
+                'caracteres_min_clave' => 8,
+                'caracteres_max_clave' => 15,
+                'caracteres_min_dni' => 8,
+                'caracteres_max_dni' => 12,
+            ]
+        ],
+        'registro' => [
+           
+        ],
+        'recuperacion' => [
+           
+        ],
+    ]
+];
