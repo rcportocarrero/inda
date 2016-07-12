24bf7585cd9775c1dc54a71cf0aedc489683f723
Carlos Miguel Sing Ramos <carlossing@gmail.com>
2016-07-12 18:01:59 -0500

    FIX

---
+++ public/js/app/_strings.js
@@ -0,0 +1,14 @@
+var _strings = {
+    app: {
+        name: '',
+        titulo: '',
+        msg_no_ini_proceso: 'El proceso no ha comenzado',
+        msg_fin_proceso: 'El proceso ha culminado',
+        msg_sistema: 'Mensaje del sistema',
+        validate: {
+            only_numbers: 'Debe llenar este campo sólo con números.',
+            only_letters: 'Debe llenar este campo sólo con letras.',
+            required: 'Debe llenar este campo.'
+        }
+    }
+}
