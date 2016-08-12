dc0a0d6cdc4e8d85fc4008d3e930d7f3bdff1125
Carlos Alvarez Chumbiauca <alvarez.5127@gmail.com>
2016-08-12 18:36:21 -0500

    fix-nro-atencion

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -8,7 +8,7 @@ return [
         'email_support1' => 'prueba1@pruebas.com',
         'email_support2' => 'prueba2@pruebas.com',
         'email_support3' => 'prueba3@pruebas.com',
-        'number_support1' => '(01) 501234',
+        'number_support1' => '(01) 615-5887',
         'number_support2' => '501234',
         'number_support3' => '501234',
         'img_logo_minedu' => '',
