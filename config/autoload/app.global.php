0ff8ab471630ead29abe4435ab5727b9f0125f2d
Carlos Alvarez Chumbiauca <alvarez.5127@gmail.com>
2016-09-02 14:25:52 -0500

    fix-config_google_analititcs

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -15,6 +15,11 @@ return [
         'img_logo_concurso' => '',
         'img_front_login' => '',
         'time_zone' => 'America/Lima',
-        'google_analytics_code' => '',
+        'google_analytics_code' => "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-62181626-9', 'auto');
+            ga('send', 'pageview');",
     ],
 ];
